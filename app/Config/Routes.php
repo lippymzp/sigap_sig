4786fd6ccf92670e7f24fb108ad5bc159a555a54
Arbiolana <nursyamsiarbiolana@gmail.com>
2026-04-13 22:29:04 +0700

    ada

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-<<<<<<< Updated upstream
 $routes->get('/', 'dasboardh::dashboard');
-=======
+
