4d233c2442d05c1ae74bdd624afd3aa64358f94b
Ahmad Irfan Nasirudin <nasirudinirfan054@gmail.com>
2026-04-24 21:01:38 +0700

    penyesuaian sidebar done(tampilan dan tombol geser burger)

--- app/Controllers/profile2.php
+++ app/Controllers/profile2.php
@@ -9,8 +9,13 @@ class Profile2 extends Controller
     public function profil_admin()
     {
         $data = [
-            'nama'  => 'Admin',
-            'email' => 'admin@gmail.com'
+            'nama'   => 'Admin',
+            'email'  => 'admin@gmail.com',
+
+            // WAJIB untuk layout
+            'menu'   => 'profil',          // untuk active sidebar
+            'judul'  => 'Profil Admin',    // untuk topbar title
+            'title'  => 'Profil Admin'     // optional (tab browser)
         ];
 
         return view('gol_a/profil_admin', $data);
