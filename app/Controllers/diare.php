7193bc09696f21f4c355bd5f6fa240e653e3a3d3
faizalsaja <faamiizlaal@gmail.com>
2026-05-20 23:34:34 +0700

    e

--- app/Controllers/diare.php
+++ app/Controllers/diare.php
@@ -95,39 +95,35 @@ $semuaJawaban = array_merge(
     // DECISION TREE
     // =========================
    $tree = new DiareDecisionTree();
+$tree = new DiareDecisionTree();
 $prediksi = $tree->predict($semuaJawaban);
 
 $statusDiare = $prediksi['diare'];
 $statusDehidrasi = $prediksi['dehidrasi'];
 
-$hasil = $statusDiare . ' | Dehidrasi: ' . $statusDehidrasi;
+$hasil = $statusDiare;
+$dehidrasi = $statusDehidrasi;
 
-$warna = 'info';
-
-if ($statusDiare === 'Tidak Diare') {
-    $rekomendasi = 'Gejala Anda tidak memenuhi kriteria diare. Tetap jaga hidrasi, pola makan sehat, dan pantau kondisi tubuh.';
-}
-
-elseif ($statusDehidrasi === 'Berat') {
-    $warna = 'danger';
-    $rekomendasi = 'Terdapat indikasi dehidrasi berat. Segera ke fasilitas kesehatan untuk penanganan medis dan rehidrasi intensif.';
-}
-
-elseif ($statusDehidrasi === 'Sedang') {
-    $warna = 'warning';
-    $rekomendasi = 'Terdapat indikasi dehidrasi sedang. Disarankan rehidrasi oral menggunakan oralit, banyak minum, dan observasi kondisi.';
-}
-
-elseif ($statusDehidrasi === 'Ringan') {
-    $warna = 'primary';
-    $rekomendasi = 'Terdapat indikasi dehidrasi ringan. Perbanyak cairan, istirahat cukup, dan konsumsi makanan yang mudah dicerna.';
-}
-
-else {
+if ($statusDiare === 'Tidak') {
     $warna = 'success';
-    $rekomendasi = 'Anda mengalami diare tanpa tanda dehidrasi signifikan. Tetap jaga cairan tubuh dan pola makan sehat.';
+    $rekomendasi = 'Tidak ditemukan indikasi diare. Tetap jaga pola hidup sehat dan hidrasi tubuh.';
+}
+elseif ($statusDiare === 'Ringan') {
+    $warna = 'info';
+    $rekomendasi = 'Gejala mengarah ke diare ringan. Perbanyak minum, istirahat, dan konsumsi makanan ringan.';
+}
+elseif ($statusDiare === 'Sedang') {
+    $warna = 'warning';
+    $rekomendasi = 'Gejala mengarah ke diare sedang. Disarankan oralit, hidrasi cukup, dan observasi kondisi.';
+}
+else {
+    $warna = 'danger';
+    $rekomendasi = 'Gejala mengarah ke diare berat. Segera ke fasilitas kesehatan.';
 }
 
+if ($dehidrasi === 'Iya') {
+    $rekomendasi .= ' Terdapat tanda dehidrasi, segera tingkatkan asupan cairan.';
+}
 $pasienModel = new PasienSkriningModel();
 $skriningModel = new SkriningModel();
 /*
@@ -190,12 +186,13 @@ $skriningModel->insert([
     ]);
 
     return view('gol_d/hasil_diare', [
-        'identitas'   => $identitas,
-        'jawaban'     => $semuaJawaban,
-        'hasil'       => $hasil,
-        'warna'       => $warna,
-        'rekomendasi' => $rekomendasi
-    ]);
+    'identitas'   => $identitas,
+    'jawaban'     => $semuaJawaban,
+    'hasil'       => $hasil,
+    'dehidrasi'   => $dehidrasi,
+    'warna'       => $warna,
+    'rekomendasi' => $rekomendasi
+]);
 }
 
     // =========================
