ef4fbf6db2d8400e004fb8d32d9aa3a0ebae0728
Hangga <naufalaryarunu@gmail.com>
2026-05-23 00:53:52 +0700

    bug kecamatan
    done

--- app/Views/gol_c/pneumonia.php
+++ app/Views/gol_c/pneumonia.php
@@ -1943,7 +1943,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         document.getElementById("detailTitleHeader").innerText = "Peta Sebaran Kasus " + selectedDetailYear;
         document.getElementById("detailYear").innerText = selectedDetailYear;
-        document.getElementById("detailWilayah").innerText = "Kecamatan " + namaWilayah;
+        document.getElementById("detailWilayah").innerText = "Kelurahan " + namaWilayah;
         document.getElementById("detailTotal").innerText = item.total + " kasus";
 
         if(bulan){
