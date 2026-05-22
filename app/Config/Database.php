8a073b7dc47167284ee491a1c98fce9899d98a10
faizalsaja <faamiizlaal@gmail.com>
2026-05-22 19:19:04 +0700

    f

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'sigap',
+        'database'     => 'sigapsig,
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
